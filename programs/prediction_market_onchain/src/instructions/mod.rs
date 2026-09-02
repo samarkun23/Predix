343f20277dc2dd920875423defa20ad1a7467a1f
samarkun <samarkun4@gmail.com>
2026-09-02 21:56:04 +0530

    mod

--- programs/prediction_market_onchain/src/instructions/mod.rs
+++ programs/prediction_market_onchain/src/instructions/mod.rs
@@ -1,5 +1,11 @@
+pub mod add_liquidity;
+pub mod create_market;
 pub mod mint_shares;
-pub mod  create_market;
+pub mod resolve_market;
+pub mod swap;
 
-pub use mint_shares::*;
+pub use add_liquidity::*;
 pub use create_market::*;
+pub use mint_shares::*;
+pub use resolve_market::*;
+pub use swap::*;
