7c3f6869dd2894fc6f5f28e5a1414e84230f1734
samarkun <samarkun4@gmail.com>
2026-09-03 22:15:41 +0530

    .gitignore

--- Predix/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/var/home/samarkun/Downloads/solana/projects/prediction_market_onchain/Predix/app/layout\":[\"static/media/558ca1a6aa3cb55e-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- Predix/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/jBr258C3XdfEpFFBCo/I0rCVVKln/F99hzOGy2tza4=\"\n}"
